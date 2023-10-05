550d85e5fce4a5dcbb7aa355a831d9fdcb3af45e
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-06 02:41:35 +0600

    Title & Description Implemented On Contact Us Page

--- client/src/pages/ContactPage/Contact.jsx
+++ client/src/pages/ContactPage/Contact.jsx
@@ -1,9 +1,14 @@
 const Contact = () => {
-    return (
-        <div>
-            Contact
-        </div>
-    );
+	return (
+		<section>
+			<div className="container">
+				<h2 className="heading text-center">Contact Us</h2>
+				<p className="text__para mb-8 lg:mb-16 font-light text-center">
+					Got a technical issue? Want to send feedback about a beta feaure? Let us know.
+				</p>
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
