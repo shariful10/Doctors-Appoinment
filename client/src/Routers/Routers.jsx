5bfacef80cf88a34f1992232812b07847234b9f1
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 11:55:45 +0600

    Hero Section Added

--- client/src/Routers/Routers.jsx
+++ client/src/Routers/Routers.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/HomePage/Home";
-import Contact from "../pages/Contact";
-import Services from "../pages/Services";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup";
-import Doctors from "../pages/Doctors/Doctors";
-import DoctorDetails from "../pages/Doctors/DoctorDetails";
+import Home from "../pages/HomePage/Home/Home";
+// import Contact from "../pages/Contact";
+// import Services from "../pages/Services";
+// import Login from "../pages/Login";
+// import Signup from "../pages/Signup";
+// import Doctors from "../pages/Doctors/Doctors";
+// import DoctorDetails from "../pages/Doctors/DoctorDetails";
 export const router = createBrowserRouter([
 	{
 		path: "/",
@@ -16,30 +16,30 @@ export const router = createBrowserRouter([
 				path: "/",
 				element: <Home />,
 			},
-			{
-				path: "/contact",
-				element: <Contact />,
-			},
-			{
-				path: "/services",
-				element: <Services />,
-			},
-			{
-				path: "/doctors",
-				element: <Doctors />,
-			},
-			{
-				path: "/doctors/:id",
-				element: <DoctorDetails />,
-			},
-			{
-				path: "/login",
-				element: <Login />,
-			},
-			{
-				path: "/signup",
-				element: <Signup />,
-			},
+			// {
+			// 	path: "/contact",
+			// 	element: <Contact />,
+			// },
+			// {
+			// 	path: "/services",
+			// 	element: <Services />,
+			// },
+			// {
+			// 	path: "/doctors",
+			// 	element: <Doctors />,
+			// },
+			// {
+			// 	path: "/doctors/:id",
+			// 	element: <DoctorDetails />,
+			// },
+			// {
+			// 	path: "/login",
+			// 	element: <Login />,
+			// },
+			// {
+			// 	path: "/signup",
+			// 	element: <Signup />,
+			// },
 		],
 	},
 ]);
