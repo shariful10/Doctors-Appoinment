dc4c2e1a70fdb74cab3dfedba23816ddc2e2f9e6
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 12:02:22 +0600

    Medical Services Addded On Home Page

--- client/src/pages/HomePage/Home/Home.jsx
+++ client/src/pages/HomePage/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeroSection from "../HeroSection/HeroSection";
+import MedicalService from "../MedicalService/MedicalService";
 
 const Home = () => {
 	return (
 		<>
 			<HeroSection />
+			<MedicalService />
 		</>
 	);
 };
