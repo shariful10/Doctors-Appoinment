35dc4efe193607b7b6c3834abc327cd42144ee97
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 22:01:37 +0600

    Specific Doctor Route Completed

--- client/src/pages/DoctorsPage/DoctorDetails.jsx
+++ client/src/pages/DoctorsPage/DoctorDetails.jsx
@@ -14,7 +14,7 @@ const DoctorDetails = () => {
 				<div className="grid md:grid-cols-3 gap-[50px]">
 					<div className="md:col-span-2">
 						{/* <======<<======= First Column =======>>======> */}
-						<div className="flex items-center gap-5">
+						<div className="flex items-center gap-5 mb-5">
 							<figure className="max-w-[200px] max-h-[200px]">
 								<img src={doctorImg} className="w-full" alt="Doctor Image" />
 							</figure>
@@ -22,10 +22,10 @@ const DoctorDetails = () => {
 								<span className="bg-[#CCF0FC] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">
 									Neurologist
 								</span>
-								<h3 className="text-headingsColor text-[22px]  leading-9 mt-3 font-bold">
+								<h3 className="text-headingsColor text-[18px] leading-6 md:text-[22px] md:leading-9 mt-3 font-bold">
 									Dr. Edward Geltman
 								</h3>
-								<div className="flex items-center gap-[6px]">
+								<div className="flex items-center gap-[6px] mt-3 md:mt-0">
 									<span className="flex items-center gap-[6px] text-[14px] leading-5 lg:text-[16px] lg:leading-7 font-semibold text-headingsColor">
 										<img src={starIcon} alt="" /> 4.8
 									</span>
@@ -33,12 +33,14 @@ const DoctorDetails = () => {
 										(272)
 									</span>
 								</div>
-								<p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px]">
-									Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit,
-									dolores?
+								<p className="text__para text-[14px] leading-5 md:text-[15px] lg:max-w-[390px] hidden md:block">
+									Dr. Edward Geltman: Renowned neurologist, 20+ years of expertise, known for compassionate care and groundbreaking research.
 								</p>
 							</div>
 						</div>
+						<p className="text__para text-[14px] leading-6 md:text-[15px] lg:max-w-[390px] md:hidden">
+							Dr. Edward Geltman: Renowned neurologist, 20+ years of expertise, known for compassionate care and groundbreaking research.
+						</p>
 						{/* <======<<======= Second Column (Tab) =======>>======> */}
 						<div className="mt-[50px] border-b border-irisBlueColor">
 							<button
@@ -66,7 +68,7 @@ const DoctorDetails = () => {
 							{tab === "feedback" && <Feedback />}
 						</div>
 					</div>
-                    {/* <---------<<-------- Side Panel --------->>---------> */}
+					{/* <---------<<-------- Side Panel --------->>---------> */}
 					<div>
 						<SidePanel />
 					</div>
--- client/src/pages/DoctorsPage/SidePanel.jsx
+++ client/src/pages/DoctorsPage/SidePanel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import BtnAppoinment from "../../components/BtnAppoinment/BtnAppoinment";
 
 const SidePanel = () => {
 	return (
@@ -11,11 +11,37 @@ const SidePanel = () => {
 			</div>
 			<div className="mt-[30px]">
 				<p className="text__para mt-0 font-semibold">Available Time Slots:</p>
-                <ul className="mt-3">
-                    <li className="flex items-center justify-between mb-2">
-                        <p className=""></p>
-                    </li>
-                </ul>
+				<ul className="mt-3">
+					<li className="flex items-center justify-between mb-2">
+						<p className="text-[15px] leading-6 text-textColor font-semibold">Sunday</p>
+						<p className="text-[15px] leading-6 text-textColor font-semibold">
+							4:00 PM - 9:30PM
+						</p>
+					</li>
+				</ul>
+				<ul className="mt-3">
+					<li className="flex items-center justify-between mb-2">
+						<p className="text-[15px] leading-6 text-textColor font-semibold">
+							Tuesday
+						</p>
+						<p className="text-[15px] leading-6 text-textColor font-semibold">
+							4:00 PM - 9:30PM
+						</p>
+					</li>
+				</ul>
+				<ul className="mt-3">
+					<li className="flex items-center justify-between mb-2">
+						<p className="text-[15px] leading-6 text-textColor font-semibold">
+							Wednesday
+						</p>
+						<p className="text-[15px] leading-6 text-textColor font-semibold">
+							4:00 PM - 9:30PM
+						</p>
+					</li>
+				</ul>
+			</div>
+			<div className="mt-5">
+				<BtnAppoinment title="Book Appoinment" />
 			</div>
 		</div>
 	);
