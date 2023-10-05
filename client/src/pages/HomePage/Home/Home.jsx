069f9a85f4a40d0da4a9e9628e5b1382a7959c18
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 12:13:12 +0600

    About Section Addded On Home Page

--- client/src/pages/HomePage/Home/Home.jsx
+++ client/src/pages/HomePage/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../HeroSection/HeroSection";
 import MedicalService from "../MedicalService/MedicalService";
+import AboutSection from "../AboutSection/AboutSection";
 
 const Home = () => {
 	return (
 		<>
 			<HeroSection />
 			<MedicalService />
+			<AboutSection />
 		</>
 	);
 };
