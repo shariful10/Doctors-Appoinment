f3e03f598372da058899430d9bc5abb8a33bb5c4
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 14:59:05 +0600

    Title & Search Field Implemented On Find a Doctor Page

--- client/src/pages/DoctorsPage/Doctor.jsx
+++ client/src/pages/DoctorsPage/Doctor.jsx
@@ -1,8 +1,13 @@
 const Doctor = () => {
     return (
-        <div>
-            Doctor
-        </div>
+        <section className="bg-[#FFF9EA]">
+            <div className="container text-center">
+                <h2 className="heading">Find a Doctor</h2>
+                <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066FF2C] rounded-md flex items-center justify-between">
+                    <input type="search" className="py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor" placeholder="Search" />
+                </div>
+            </div>
+        </section>
     );
 };
 
