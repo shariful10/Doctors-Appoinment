cceeecb7ec7fe3d85ec38113e1f3672c8a5b3fe4
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-10-05 13:38:36 +0600

    Great Doctors Section Addded On Home Page

--- client/src/pages/HomePage/Home/Home.jsx
+++ client/src/pages/HomePage/Home/Home.jsx
@@ -4,6 +4,7 @@ import MedicalService from "../MedicalService/MedicalService";
 import AboutSection from "../AboutSection/AboutSection";
 import Services from "../Services/Services";
 import Features from "../FeaturesSection/Features";
+import GreatDoctors from "../GreatDoctors/GreatDoctors";
 
 const Home = () => {
 	return (
@@ -13,6 +14,7 @@ const Home = () => {
 			<AboutSection />
 			<Services />
 			<Features />
+			<GreatDoctors />
 		</>
 	);
 };
